ad73fc74ac0e61ba65dded0bcc6ffa10ca8db9b8
Simon <simonjudd2@gmail.com>
2023-04-10 15:58:02 +0100

    feat: first commit

---
+++ agent.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python3
+import os
+import openai
+from dotenv import load_dotenv
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import DeepLake
+from langchain.chat_models import ChatOpenAI
+from langchain.chains import ConversationalRetrievalChain
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
+from langchain.chains import SequentialChain
+
+load_dotenv()
+
+# set the OpenAI API key
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY", "")
+embeddings = OpenAIEmbeddings()
+
+def agent():
+    # load the dataset
+    db = DeepLake(dataset_path="hub://davitbun/twitter-algorithm", read_only=True, embedding_function=embeddings)
+
+    retriever = db.as_retriever()
+    retriever.search_kwargs['distance_metric'] = 'cos'
+    retriever.search_kwargs['fetch_k'] = 100
+    retriever.search_kwargs['maximal_marginal_relevance'] = True
+    retriever.search_kwargs['k'] = 20
+
+    # Load model
+    model = ChatOpenAI(model_name='gpt-3.5-turbo') # 'gpt-3.5-turbo',
+    qa = ConversationalRetrievalChain.from_llm(model,retriever=retriever)
+
+    questions = [
+        "Describe at a high level how the ranking system works",
+        "What are three core components of the ranking system?",
+    ]
+    chat_history = []
+
+    for question in questions:
+        result = qa({"question": question, "chat_history": chat_history})
+        chat_history.append((question, result['answer']))
+        print(f"-> **Question**: {question} \n")
+        print(f"**Answer**: {result['answer']} \n")
+
+    # Content summary
+    summary_prompt = PromptTemplate(
+            input_variables=["chat_history", "questions"],
+            template=""" You are an agent with the task of creating an exciting short essay.  Your goal is to make the content engaging and accessible.
+            You will recieve a list of {questions} and answers {chat_history}.  All the questions are related and cover the same topic.
+            Your task it to write a short essay that summarizes the topics.  You can use the questions and answers to help you write the essay.
+
+            Each paragraph should consist or two to three complete sentences.
+            Your essay should be exactly five paragraphs long.
+
+            The first paragraph should be an introduction that cleary states the topic that will be covered and why it matters to the reader.
+            The second, third, and fourth paragraphs should each explain one key element of the topic.
+            The fifth paragraph should be a conclusion that summarizes the main points of the essay.
+
+            Do not refer to yourself.  Use the metaphor of a marketplace, tweets are the goods being sold and algorithm evaluates who sees them.
+            The essay should be written in the present tense and should be written in the active voice.
+
+            At the end of each paragraph add a "/" and the paragraph number.  For example, "/1"
+            """
+        )
+    review_prompt = PromptTemplate(
+        input_variables=["thread"],
+        template="""You are a content review and consolidation agent.  You will recieve a numbered list of paragraphs {thread}.
+        your job is make sure that each paragraph in the list is ALWAYS under 140 characts long.
+        If a paragraph is longer than 140 characters, you should split it into two or more paragraphs.
+        If you split a paragraph, you should add a "/" and the paragraph number to the end of the paragraph.
+
+        Make sure that the paragraphs are in the correct order.
+        """
+        )
+
+    summary_chain = LLMChain(llm=model, prompt=summary_prompt, output_key="thread")
+    review_chain = LLMChain(llm=model, prompt=review_prompt, output_key="review")
+
+    overall_chain = SequentialChain(chains=[summary_chain, review_chain], input_variables=["chat_history", "questions"], output_variables=["thread", "review"], verbose=True)
+    response = overall_chain({"chat_history" : chat_history, "questions": questions})
+    new_tweets = response.split("\n")
+    parsed_content = [element for element in new_tweets if element != '']
+    print(parsed_content)
+    return parsed_content
---
+++ runner.py
@@ -0,0 +1,34 @@
+import main
+import redis
+import json
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Get the Twitter API keys from the environment
+twitter = main.make_token()
+client_id = os.getenv("CLIENT_ID", "")
+client_secret = os.getenv("CLIENT_SECRET", "")
+token_url = "https://api.twitter.com/2/oauth2/token"
+
+# Save the bearer token
+t = main.r.get("token")
+bb_t = t.decode("utf8").replace("'", '"')
+data = json.loads(bb_t)
+
+refreshed_token = twitter.refresh_token(
+    client_id=client_id,
+    client_secret=client_secret,
+    token_url=token_url,
+    refresh_token=data["refresh_token"],
+)
+
+# Save the refreshed token
+st_refreshed_token = '"{}"'.format(refreshed_token)
+j_refreshed_token = json.loads(st_refreshed_token)
+main.r.set("token", j_refreshed_token)
+
+# Get the dog fact
+tweets = main.parse_codebase()
+main.post_tweet_thread(tweets, refreshed_token)
